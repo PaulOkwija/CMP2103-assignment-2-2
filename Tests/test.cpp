0bfcd7af85c26cad495222a953cb3486da329b38
Paul Mugume Okwija <okwijapaul@gmail.com>
2022-11-02 20:40:06 +0300

    Add files via upload

--- Tests/test.cpp
+++ Tests/test.cpp
@@ -1,16 +1,16 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
-#include <cmath>
-#include "../main.cpp"
-//std::fixed;
-//std::setprecision(int 2);
-float a1 = (6.0/100.0)*pow(2.71828,-1.4/(100.0*0.026));
-float a2 = (35.0/50.0)*pow(2.71828,-0.11/(50.0*0.016));
-float a3 = (15.0/20.0)*pow(2.71828,-0.066/(20.0*0.055));
-TEST_CASE( "Tests_2", "Tests" ) {
-    REQUIRE( RC_current(6,100,0.026,1.4) == a1 );
-    REQUIRE( RC_current(35,50,0.016,0.11) == a2);
+#include "../main.h"
+
+
+
+TEST_CASE( "Test_1", "Tests" ) {
+    REQUIRE( check_triangle_answer(3,4,5) == "The triangle is a right-angled triangle");
+    REQUIRE( check_triangle_answer(3,3,5) == "The triangle is an isosceles triangle");
+    REQUIRE( check_triangle_answer(3,3,3) == "The triangle is an equilateral triangle");
+    REQUIRE( check_triangle_answer(52,24,13) == "The triangle is a scalene triangle");
 }
-TEST_CASE( "Tests_1", "Tests" ) {
-    REQUIRE( RC_current(15,20,0.055,0.066) == a3);
+TEST_CASE( "Test_2", "Tests" ) {
+    REQUIRE( check_triangle_answer(5,4,3) == "The triangle is a right-angled triangle");
 }
+
--- Tests/test_2.cpp
+++ Tests/test_2.cpp
@@ -1,9 +1,20 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
-#include "../main.cpp"
+#include "../main.h"
 
-TEST_CASE( "Tests_2", "Tests_2" ) {
-    REQUIRE( RC_current(10,50.4,0.16,2.11) == 0.152733 );
-    REQUIRE( RC_current(35,50,0.016,0.11) == 0.00516651);
+string Eq_triangle = "The triangle is an Equilateral triangle";
+string Iso_triangle = "The triangle is an Isosceles triangle";
+string Right_ang_triangle = "The triangle is an Right-angled triangle";
+string Sca_triangle = "The triangle is an Scalene triangle";
+
+TEST_CASE( "Tests_2", "Tests" ) {
+    REQUIRE( check_triangle(3,4,5) == Right_ang_triangle );
+    REQUIRE( check_triangle(3,3,5) == Iso_triangle);
+    REQUIRE( check_triangle(3,3,3) == Eq_triangle);
+    REQUIRE( check_triangle(5,4,3) == Right_ang_triangle);
+    REQUIRE( check_triangle(52,24,13) == Sca_triangle);
+}
+TEST_CASE( "Tests_1", "Tests" ) {
+    REQUIRE( check_triangle(3,3,5) == Iso_triangle);
 }
 
