f02f5a2241ba5ec82aaaf13e377431d8de6b6780
Paul Mugume Okwija <okwijapaul@gmail.com>
2022-11-02 21:27:30 +0300

    Update test_2.cpp

--- Tests/test_2.cpp
+++ Tests/test_2.cpp
@@ -2,19 +2,19 @@
 #include "catch.hpp"
 #include "../main.h"
 
-string Eq_triangle = "The triangle is an equilateral triangle";
-string Iso_triangle = "The triangle is an isosceles triangle";
-string Right_ang_triangle = "The triangle is an right-angled triangle";
-string Sca_triangle = "The triangle is an scalene triangle";
+std::string Eq_triangle = "The triangle is an equilateral triangle";
+std::string Iso_triangle = "The triangle is an isosceles triangle";
+std::string Right_ang_triangle = "The triangle is a right-angled triangle";
+std::string Sca_triangle = "The triangle is a scalene triangle";
 
 TEST_CASE( "Tests_3", "Tests" ) {
-    REQUIRE( check_triangle(10,8,6) == Right_ang_triangle );
-    REQUIRE( check_triangle(13,13,25) == Iso_triangle);
-    REQUIRE( check_triangle(3,31,30) == Eq_triangle);
-    REQUIRE( check_triangle(15,9,12) == Right_ang_triangle);
-    REQUIRE( check_triangle(2,4,3) == Sca_triangle);
+    REQUIRE(check_triangle(10,8,6) == Right_ang_triangle);
+    REQUIRE(check_triangle(13,13,25) == Iso_triangle);
+    REQUIRE(check_triangle(3,31,30) == Sca_triangle);
+    REQUIRE(check_triangle(15,9,12) == Right_ang_triangle);
+    REQUIRE(check_triangle(2,4,3) == Sca_triangle);
 }
 TEST_CASE( "Tests_4", "Tests" ) {
-    REQUIRE( check_triangle(23,13,5) == Iso_triangle);
+    REQUIRE( check_triangle(8,10,6) == Right_ang_triangle);
 }
 
