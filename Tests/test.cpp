04d05f071ece461704507af35d13e4eafee6a321
Paul Mugume Okwija <okwijapaul@gmail.com>
2022-11-02 21:13:53 +0300

    Update test.cpp

--- Tests/test.cpp
+++ Tests/test.cpp
@@ -5,10 +5,10 @@
 
 
 TEST_CASE( "Test_1", "Tests" ) {
-    REQUIRE( check_triangle_answer(3,4,5) == "The triangle is a right-angled triangle");
-    REQUIRE( check_triangle_answer(3,3,5) == "The triangle is an isosceles triangle");
-    REQUIRE( check_triangle_answer(3,3,3) == "The triangle is an equilateral triangle");
-    REQUIRE( check_triangle_answer(52,24,13) == "The triangle is a scalene triangle");
+    REQUIRE( check_triangle(3,4,5) == "The triangle is a right-angled triangle");
+    REQUIRE( check_triangle(3,3,5) == "The triangle is an isosceles triangle");
+    REQUIRE( check_triangle(3,3,3) == "The triangle is an equilateral triangle");
+    REQUIRE( check_triangle(52,24,13) == "The triangle is a scalene triangle");
 }
 TEST_CASE( "Test_2", "Tests" ) {
     REQUIRE( check_triangle_answer(5,4,3) == "The triangle is a right-angled triangle");
