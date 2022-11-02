06a3839f9aeca78343c07ac9ac0f5749196738ed
Paul Mugume Okwija <okwijapaul@gmail.com>
2022-11-02 21:29:04 +0300

    Update main.h

--- main.h
+++ main.h
@@ -6,22 +6,5 @@
 using namespace std;
 
 string check_triangle(double a, double b, double c){
-    double a_squared;
-    double b_squared;
-    double c_squared;
-    a_squared = pow(b,2) + pow(c,2);
-    b_squared = pow(a,2) + pow(c,2);
-    c_squared = pow(a,2) + pow(b,2);
-    if (a==b and b==c)
-        return "The triangle is an equilateral triangle";
-
-    else if(a==b or b==c)
-        return "The triangle is an isosceles triangle";
-
-    else if(pow(a,2)== a_squared or pow(b,2)== b_squared or pow(c,2)== c_squared)
-        return "The triangle is a right-angled triangle";
-
-    else
-        return "The triangle is a scalene triangle";
-}
+    }
 #endif // MAIN_H
