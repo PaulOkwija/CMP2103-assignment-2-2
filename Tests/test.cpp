fb95a9486264abed20d0773aefab6c72e895cf7c
Paul Mugume Okwija <okwijapaul@gmail.com>
2022-11-05 17:37:06 +0300

    Update test.cpp

--- Tests/test.cpp
+++ Tests/test.cpp
@@ -3,14 +3,18 @@
 #include "../main.h"
 
 
+std::string Eq_triangle = "The triangle is an equilateral triangle";
+std::string Iso_triangle = "The triangle is an isosceles triangle";
+std::string Right_ang_triangle = "The triangle is a right-angled triangle";
+std::string Sca_triangle = "The triangle is a scalene triangle";
 
 TEST_CASE( "Test_1", "Tests" ) {
-    REQUIRE( check_triangle(3,4,5) == "The triangle is a right-angled triangle");
-    REQUIRE( check_triangle(3,3,5) == "The triangle is an isosceles triangle");
-    REQUIRE( check_triangle(3,3,3) == "The triangle is an equilateral triangle");
-    REQUIRE( check_triangle(52,24,13) == "The triangle is a scalene triangle");
+    REQUIRE( check_triangle(3,4,5) == Right_ang_triangle);
+    REQUIRE( check_triangle(3,3,5) == Iso_triangle);
+    REQUIRE( check_triangle(3,3,3) == Eq_triangle);
+    REQUIRE( check_triangle(52,24,13) == Sca_triangle);
 }
 TEST_CASE( "Test_2", "Tests" ) {
-    REQUIRE( check_triangle(5,4,3) == "The triangle is a right-angled triangle");
+    REQUIRE( check_triangle(5,4,3) == Right_ang_triangle);
 }
 
